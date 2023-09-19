8a9aeab7b542ed0d44d0da84d118a342f6586bcb
Moritz Sallermann <m.sallermann@fz-juelich.de>
2023-09-19 21:17:34 +0000

    Added command line argument parser.
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,37 @@
-#include <iostream>
 #include <fmt/core.h>
+#include <iostream>
 
 #include "state.hpp"
 
+#include <argparse/argparse.hpp>
+#include <string>
+
 #include "models/DeGroot.hpp"
 
-int main()
+int main( int argc, char * argv[] )
 {
-    auto state = Seldon::State("conf.toml");
-    auto model = Seldon::DeGrootModel(state);
-    model.run();
+
+    argparse::ArgumentParser program( "seldon" );
+
+    program.add_argument( "config_file" ).help( "The config file to be used. Has to be in TOML format." );
+
+    try
+    {
+        program.parse_args( argc, argv );
+    }
+    catch( const std::runtime_error & err )
+    {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        return 1;
+    }
+
+    auto config_file_path = program.get<std::string>( "config_file" );
+    fmt::print( "Using input file: {}\n", config_file_path );
+
+    // auto state = Seldon::State("conf.toml");
+    // auto model = Seldon::DeGrootModel(state);
+    // model.run();
+
+    return 0;
 }
