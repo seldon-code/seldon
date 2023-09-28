21fe672e07e586b7c5763e67131c68af60b115dd
Moritz Sallermann <m.sallermann@fz-juelich.de>
2023-09-28 22:33:10 +0000

    Changed specification of output directory.
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main( int argc, char * argv[] )
     program.add_argument( "config_file" ).help( "The config file to be used. Has to be in TOML format." );
 
     // Output directory location
-    program.add_argument( "-o", "--output" ).help( "Specify the output directory location." );
+    program.add_argument( "-o", "--output" ).help( "Specify the output directory." );
 
     try
     {
@@ -30,35 +30,33 @@ int main( int argc, char * argv[] )
 
     fs::path config_file_path = program.get<std::string>( "config_file" );
     fmt::print( "Using input file: {}\n", config_file_path.string() );
-    fs::path output_parent_path; // Output directory parent file path
-    fs::path output_dir_path;    // All outputs will be saved to a directory named output
+    fs::path output_dir_path; // All outputs will be saved to a directory named output
 
     // If the output directory has not been specified,
     // set it to the parent directory where the config file is present
     if( auto fn = program.present( "-o" ) )
     {
-        output_parent_path = *fn;
+        output_dir_path = *fn;
     } // set the output directory path
     else
     {
-        output_parent_path = config_file_path.parent_path();
-        fmt::print( "Output directory parent path set to: {}\n", output_parent_path.string() );
+        output_dir_path = config_file_path.parent_path() / fs::path( "output" );
+        fmt::print( "Output directory path set to: {}\n", output_dir_path.string() );
     } // if the output directory has not been provided
 
     // Get the output directory path
-    output_dir_path = output_parent_path / fs::path( "output" );
-    fs::create_directory( output_dir_path ); // Create the output directory
+    fs::create_directories( output_dir_path ); // Create the output directory
 
     auto simulation = Seldon::Simulation( config_file_path.string() );
 
     Seldon::IO::network_to_dot_file( simulation.network, ( output_dir_path / fs::path( "network.dot" ) ).string() );
 
-    auto filename = fmt::format( "simulation{}.txt", 0 );
+    auto filename = fmt::format( "iteration_{}.txt", 0 );
     Seldon::IO::simulation_state_to_file( simulation, ( output_dir_path / fs::path( filename ) ).string() );
     for( size_t i = 1; i < 10; i++ )
     {
         simulation.model->run();
-        filename = fmt::format( "simulation{}.txt", i );
+        filename = fmt::format( "iteration_{}.txt", i );
         Seldon::IO::simulation_state_to_file( simulation, ( output_dir_path / fs::path( filename ) ).string() );
     }
 
