72f278fa21a88cdac6b4ca531f0c9c0abba629e5
amritagos <amrita16thaug646@gmail.com>
2023-09-30 14:01:23 +0000

    tarjan: Started writing a test for Tarjan

--- test/test_tarjan.cpp
+++ test/test_tarjan.cpp
@@ -1,14 +1,14 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include <cstdint>
+#include "connectivity.hpp"
+#include <vector>
 
-uint32_t factorial( uint32_t number ) {
+void create_neighbour_list( uint32_t number ) {
     return number <= 1 ? number : factorial(number-1) * number;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
+TEST_CASE( "Test for Tarjan's algorithm for strongly connected networks", "[tarjan]" ) {
     REQUIRE( factorial( 1) == 1 );
     REQUIRE( factorial( 2) == 2 );
     REQUIRE( factorial( 3) == 6 );
-    REQUIRE( factorial(10) != 3'628'800 );
 }
